a5bc93f342a54e717f5bcc672478b2a8f9b9154b
Rob Lathrop <rl@polyswarm.io>
2020-09-16 18:32:22 -0400

    Fix middleware

--- src/microenginewebhookspy/middleware.py
+++ src/microenginewebhookspy/middleware.py
@@ -1,22 +1,32 @@
 import hmac
-import os
+import logging
+from io import BytesIO
 
-API_KEY = os.environ.get('API_KEY')
+from microenginewebhookspy.settings import API_KEY
+
+logger = logging.getLogger(__name__)
 
 
 class ValidateSenderMiddleware:
-   def __init__(self, app):
-       self.app = app
+    def __init__(self, app):
+        self.app = app
 
-   def __call__(self, environ, start_response):
-       wsgi_input = environ['wsgi.input'].read()
-       signature = environ['HTTP_X_POLYSWARM_SIGNATURE'].read()
-       if self.__valid_signature(wsgi_input, signature, API_KEY):
-           return self.app(environ, start_response)
-       else:
-           return start_response("401 Not Authorized", [])
+    def __call__(self, environ, start_response):
+        wsgi_input = environ['wsgi.input'].read()
+        try:
+            signature = environ['HTTP_X_POLYSWARM_SIGNATURE']
+        except KeyError:
+            start_response("400 Bad Request", [('Content-Length', '0')])
+            return []
 
-   @staticmethod
-   def __valid_signature(body, signature, api_key):
-       digest = hmac.new(api_key.encode('utf-8'), body).hexdigest()
-       return hmac.compare_digest(digest, signature)
+        if self.__valid_signature(wsgi_input, signature, API_KEY):
+            environ['wsgi.input'] = BytesIO(wsgi_input)
+            return self.app(environ, start_response)
+        else:
+            start_response("401 Not Authorized", [('Content-Length', '0')])
+            return []
+
+    @staticmethod
+    def __valid_signature(body, signature, api_key):
+        digest = hmac.new(api_key.encode('utf-8'), body, digestmod="sha256").hexdigest()
+        return hmac.compare_digest(digest, signature)
