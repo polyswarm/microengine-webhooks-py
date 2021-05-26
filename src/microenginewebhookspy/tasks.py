640c9ef4ce2833311ced2542aea4b556affd986f
Alan Justino <alan.justino@yahoo.com.br>
2021-05-26 18:13:55 -0300

    Provide a bid on every assertion, even suspicious & unknown ones
    Suspicious & Unknown are not biddable. Just setting the bid to 0
    to have it accepted as the field is required there.

--- src/microenginewebhookspy/tasks.py
+++ src/microenginewebhookspy/tasks.py
@@ -11,9 +11,6 @@ celery_app = Celery('tasks', broker=settings.BROKER)
 def handle_bounty(bounty):
     bounty = Bounty(**bounty)
     scan_result = scan(bounty)
-    bid = None
-
-    if scan_result.verdict == Verdict.MALICIOUS or scan_result.verdict == Verdict.BENIGN:
-        bid = compute_bid(bounty, scan_result)
+    bid = compute_bid(bounty, scan_result)
 
     bounty.post_assertion(scan_result.to_assertion(bid))
