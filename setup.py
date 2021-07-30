1552faeb4e7aa23a5897f647c6d01cf3f432388f
Robert Lathrop <Rizato@users.noreply.github.com>
2021-07-30 09:34:47 -0400

    Add optional datadog metrics (#30)
    * Add microengine-utils dependency
    
    * Add metrics collection
    
    Reports only when the DATADOG_API_KEY, and DATADOG_APP_KEY are set.
    Otherwise, the metrics do nothing, so users that don't use datadog won't have to sepend time removing it.
    
    * Add Datadog values to charts
    
    * Update name
    
    * Fix whitespace
    
    * Fix template name
    
    * Add polyWork value
    
    * Match tags to existing tags
    
    * Fix whitespace

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
     install_requires=[
         "celery",
         "Flask",
+        "microengine-utils",
         "polyswarm-artifact",
         "requests",
         "python-json-logger"
--- src/microenginewebhookspy/tasks.py
+++ src/microenginewebhookspy/tasks.py
@@ -1,16 +1,69 @@
-from celery import Celery
+from celery import Celery, Task
+from microengine_utils import errors
+from microengine_utils.datadog import configure_metrics
+from microengine_utils.constants import SCAN_FAIL, SCAN_SUCCESS, SCAN_TIME, SCAN_VERDICT
 
 from microenginewebhookspy.models import Bounty, ScanResult, Verdict, Assertion, Phase
 from microenginewebhookspy import settings
 from microenginewebhookspy.scan import scan, compute_bid
 
+
 celery_app = Celery('tasks', broker=settings.BROKER)
 
 
-@celery_app.task
+class MetricsTask(Task):
+    _metrics = None
+
+    @property
+    def metrics(self):
+        if self._metrics is None:
+            self._metrics = configure_metrics(
+                settings.DATADOG_API_KEY,
+                settings.DATADOG_APP_KEY,
+                settings.ENGINE_NAME,
+                poly_work=settings.POLY_WORK
+            )
+
+        return self._metrics
+
+
+@celery_app.task(base=MetricsTask)
 def handle_bounty(bounty):
     bounty = Bounty(**bounty)
-    scan_result = scan(bounty)
+    with handle_bounty.metrics.timer(SCAN_TIME):
+        try:
+            scan_result = scan(bounty)
+            handle_bounty.metrics.increment(SCAN_SUCCESS, tags=[f'type:{bounty.artifact_type}'])
+            handle_bounty.metrics.increment(SCAN_VERDICT, tags=[f'type:{bounty.artifact_type}',
+                                                                f'verdict:{scan_result.verdict.value}'])
+        except errors.HighCompressionScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: High Compression']
+            )
+        except errors.EncryptedFileScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: File encrypted']
+            )
+        except errors.CorruptFileScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: File corrupted']
+            )
+        except errors.FileSkippedScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: File Skipped']
+            )
+        except errors.ServerNotReadyScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: Server not ready']
+            )
+        except errors.CalledProcessScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: Error running scan process']
+            )
+        except errors.UnprocessableScanError:
+            handle_bounty.metrics.increment(
+                SCAN_FAIL, tags=[bounty.artifact_type, 'scan_error: Unprocessable']
+            )
 
     if bounty.phase == Phase.ARBITRATION:
         scan_response = scan_result.to_vote()
