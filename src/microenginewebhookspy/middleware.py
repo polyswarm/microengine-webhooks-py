43692c60f05f5f16076c894a19f6e7d3c9c3e419
Nick Davis <nick.davis@polyswarm.io>
2021-09-01 19:26:59 -0400

    more debugging. try to encode the 'body' and use 'hashlib.sha256'

--- src/microenginewebhookspy/middleware.py
+++ src/microenginewebhookspy/middleware.py
@@ -1,3 +1,4 @@
+import hashlib
 import hmac
 import json
 import logging
@@ -40,7 +41,7 @@ class ValidateSenderMiddleware:
 
     @staticmethod
     def _valid_signature(body, signature, secret):
-        digest = hmac.new(secret.encode('utf-8'), body, digestmod="sha256").hexdigest()
+        digest = hmac.new(secret.encode('utf-8'), body.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
         logger.debug('Comparing computed digest "%s" (%d) vs given signature "%s" (%d)', digest, len(digest), signature, len(signature))
-        logger.debug(b'Signature in bytes "%b" (%d)', signature.encode(), len(signature.encode()))
+        logger.debug(b'Signature in bytes "%r" (%d)', signature.encode(), len(signature.encode()))
         return hmac.compare_digest(digest, signature)
