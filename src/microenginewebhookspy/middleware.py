60b084bbcdc0c35450fb89278aed7e5d770bf8ee
Nick Davis <nick.davis@polyswarm.io>
2021-09-01 20:08:14 -0400

    try the assert to verify that values are both ascii.

--- src/microenginewebhookspy/middleware.py
+++ src/microenginewebhookspy/middleware.py
@@ -23,7 +23,7 @@ class ValidateSenderMiddleware:
         wsgi_input = environ['wsgi.input'].read()
         try:
             signature = environ['HTTP_X_POLYSWARM_SIGNATURE']
-            logger.debug('Type signature: %s', type(signature))
+            logger.debug('Type signature: %r', type(signature).__name__)
         except KeyError:
             message = json.dumps({"X-POLYSWARM-SIGNATURE": "Signature not included in headers"}).encode('utf-8')
             start_response("400 Bad Request",
@@ -43,5 +43,17 @@ class ValidateSenderMiddleware:
     def _valid_signature(body, signature, secret):
         digest = hmac.new(secret.encode('utf-8'), body, digestmod=hashlib.sha256).hexdigest()
         logger.debug('Comparing computed digest "%s" (%d) vs given signature "%s" (%d)', digest, len(digest), signature, len(signature))
-        logger.debug(b'Signature in bytes "%r" (%d)', signature.encode(), len(signature.encode()))
+        assert _is_ascii(digest)
+        assert _is_ascii(signature)
         return hmac.compare_digest(digest, signature)
+
+
+def _is_ascii(digest):
+    try:
+        digest.encode('ascii')
+    except UnicodeEncodeError:
+        logger.debug('Digest %s is not ascii', digest)
+        return False
+    else:
+        return True
+
